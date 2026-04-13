52fbe7edc03c6c778dbe4a9108d24882dd4071de
shrushti-pg <sshrushtiganiger@gmail.com>
2026-04-13 11:56:15 +0530

    added default from email

--- F2C/settings.py
+++ F2C/settings.py
@@ -134,6 +134,8 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
