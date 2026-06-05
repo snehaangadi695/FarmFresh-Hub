07fa85d3998ec7da51ad9ac4158a43bc7f9fd94b
shrushti-pg <sshrushtiganiger@gmail.com>
2026-06-05 21:03:04 +0530

    Added email debugging

--- F2C/settings.py
+++ F2C/settings.py
@@ -151,7 +151,7 @@ CLOUDINARY_STORAGE = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend''
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
